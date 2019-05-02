a54ab4da0cf22abeff116c835353a6471db7226f
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-05-02 15:47:23 +0000

    make check.sh

---
+++ check.sh
@@ -0,0 +1,9 @@
+git pull origin master
+cd /home/isucon/torb/webapp/go
+make
+sudo systemctl restart torb.go.service
+cd /home/isucon/torb/bench
+./bin/bench -output=./result.json
+jq < ./result.json
+
+cd /home/isucon/torb
