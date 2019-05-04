8840643dbc23d64acd899492d9f6b6ade7fb4adf
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-05-05 02:13:45 +0900

    myisam -> Innodb

--- db/schema.sql
+++ db/schema.sql
@@ -1,113 +1,113 @@
--- CREATE TABLE IF NOT EXISTS users (
---     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
---     nickname    VARCHAR(128) NOT NULL,
---     login_name  VARCHAR(128) NOT NULL,
---     pass_hash   VARCHAR(128) NOT NULL,
---     UNIQUE KEY login_name_uniq (login_name)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- CREATE TABLE IF NOT EXISTS events (
---     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
---     title       VARCHAR(128)     NOT NULL,
---     public_fg   TINYINT(1)       NOT NULL,
---     closed_fg   TINYINT(1)       NOT NULL,
---     price       INTEGER UNSIGNED NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- CREATE TABLE IF NOT EXISTS sheets (
---     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
---     `rank`      VARCHAR(128)     NOT NULL,
---     num         INTEGER UNSIGNED NOT NULL,
---     price       INTEGER UNSIGNED NOT NULL,
---     UNIQUE KEY rank_num_uniq (`rank`, num)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- CREATE TABLE IF NOT EXISTS reservations (
---     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
---     event_id    INTEGER UNSIGNED NOT NULL,
---     sheet_id    INTEGER UNSIGNED NOT NULL,
---     user_id     INTEGER UNSIGNED NOT NULL,
---     reserved_at DATETIME(6)      NOT NULL,
---     canceled_at DATETIME(6)      DEFAULT NULL,
---     KEY event_id_and_sheet_id_idx (event_id, sheet_id)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- CREATE TABLE IF NOT EXISTS administrators (
---     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
---     nickname    VARCHAR(128) NOT NULL,
---     login_name  VARCHAR(128) NOT NULL,
---     pass_hash   VARCHAR(128) NOT NULL,
---     UNIQUE KEY login_name_uniq (login_name)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE
-IF NOT EXISTS users
-(
+CREATE TABLE IF NOT EXISTS users (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nickname    VARCHAR
-(128) NOT NULL,
-    login_name  VARCHAR
-(128) NOT NULL,
-    pass_hash   VARCHAR
-(128) NOT NULL,
-    UNIQUE KEY login_name_uniq
-(login_name)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    nickname    VARCHAR(128) NOT NULL,
+    login_name  VARCHAR(128) NOT NULL,
+    pass_hash   VARCHAR(128) NOT NULL,
+    UNIQUE KEY login_name_uniq (login_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE
-IF NOT EXISTS events
-(
+CREATE TABLE IF NOT EXISTS events (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    title       VARCHAR
-(128)     NOT NULL,
-    public_fg   TINYINT
-(1)       NOT NULL,
-    closed_fg   TINYINT
-(1)       NOT NULL,
+    title       VARCHAR(128)     NOT NULL,
+    public_fg   TINYINT(1)       NOT NULL,
+    closed_fg   TINYINT(1)       NOT NULL,
     price       INTEGER UNSIGNED NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE
-IF NOT EXISTS sheets
-(
+CREATE TABLE IF NOT EXISTS sheets (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    `rank`      VARCHAR
-(128)     NOT NULL,
+    `rank`      VARCHAR(128)     NOT NULL,
     num         INTEGER UNSIGNED NOT NULL,
     price       INTEGER UNSIGNED NOT NULL,
-    UNIQUE KEY rank_num_uniq
-(`rank`, num)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    UNIQUE KEY rank_num_uniq (`rank`, num)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE
-IF NOT EXISTS reservations
-(
+CREATE TABLE IF NOT EXISTS reservations (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     event_id    INTEGER UNSIGNED NOT NULL,
     sheet_id    INTEGER UNSIGNED NOT NULL,
     user_id     INTEGER UNSIGNED NOT NULL,
-    reserved_at DATETIME
-(6)      NOT NULL,
-    canceled_at DATETIME
-(6)      DEFAULT NULL,
-    KEY event_id_and_sheet_id_idx
-(event_id, sheet_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    reserved_at DATETIME(6)      NOT NULL,
+    canceled_at DATETIME(6)      DEFAULT NULL,
+    KEY event_id_and_sheet_id_idx (event_id, sheet_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE
-IF NOT EXISTS administrators
-(
+CREATE TABLE IF NOT EXISTS administrators (
     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nickname    VARCHAR
-(128) NOT NULL,
-    login_name  VARCHAR
-(128) NOT NULL,
-    pass_hash   VARCHAR
-(128) NOT NULL,
-    UNIQUE KEY login_name_uniq
-(login_name)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    nickname    VARCHAR(128) NOT NULL,
+    login_name  VARCHAR(128) NOT NULL,
+    pass_hash   VARCHAR(128) NOT NULL,
+    UNIQUE KEY login_name_uniq (login_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- CREATE TABLE
+-- IF NOT EXISTS users
+-- (
+--     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     nickname    VARCHAR
+-- (128) NOT NULL,
+--     login_name  VARCHAR
+-- (128) NOT NULL,
+--     pass_hash   VARCHAR
+-- (128) NOT NULL,
+--     UNIQUE KEY login_name_uniq
+-- (login_name)
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- CREATE TABLE
+-- IF NOT EXISTS events
+-- (
+--     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     title       VARCHAR
+-- (128)     NOT NULL,
+--     public_fg   TINYINT
+-- (1)       NOT NULL,
+--     closed_fg   TINYINT
+-- (1)       NOT NULL,
+--     price       INTEGER UNSIGNED NOT NULL
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- CREATE TABLE
+-- IF NOT EXISTS sheets
+-- (
+--     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     `rank`      VARCHAR
+-- (128)     NOT NULL,
+--     num         INTEGER UNSIGNED NOT NULL,
+--     price       INTEGER UNSIGNED NOT NULL,
+--     UNIQUE KEY rank_num_uniq
+-- (`rank`, num)
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- CREATE TABLE
+-- IF NOT EXISTS reservations
+-- (
+--     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     event_id    INTEGER UNSIGNED NOT NULL,
+--     sheet_id    INTEGER UNSIGNED NOT NULL,
+--     user_id     INTEGER UNSIGNED NOT NULL,
+--     reserved_at DATETIME
+-- (6)      NOT NULL,
+--     canceled_at DATETIME
+-- (6)      DEFAULT NULL,
+--     KEY event_id_and_sheet_id_idx
+-- (event_id, sheet_id)
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- CREATE TABLE
+-- IF NOT EXISTS administrators
+-- (
+--     id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     nickname    VARCHAR
+-- (128) NOT NULL,
+--     login_name  VARCHAR
+-- (128) NOT NULL,
+--     pass_hash   VARCHAR
+-- (128) NOT NULL,
+--     UNIQUE KEY login_name_uniq
+-- (login_name)
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE reservations ADD INDEX event_id_idx(event_id);
 ALTER TABLE reservations ADD INDEX sheet_id_idx(sheet_id);
